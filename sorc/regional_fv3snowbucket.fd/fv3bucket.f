6ee2eca903d37d34b528c121121929954d232f2a
matthew pyle <Matthew.Pyle@noaa.gov>
2020-01-16 16:41:58 +0000

    Big item is possible ultimate fix for fv3snowbucket failures.  Tweaks to AK time stepping.

--- sorc/regional_fv3snowbucket.fd/fv3bucket.f
+++ sorc/regional_fv3snowbucket.fd/fv3bucket.f
@@ -29,7 +29,7 @@
 	n=index(dirname,' ')-1
 	m=index(filename,' ')-1
 
-	do I=2,2
+	I=2
 	
 	file1= dirname(1:n)//'/'//filename(1:m)
      +					//HRS(I-1)//'.grib2'
@@ -69,7 +69,8 @@
      &                  TESTOUT(1:mmm),
      &                  pdiff,reset_flag,ihrs1,interv,IM,JM)
 
-	enddo
+        write(0,*) 'past calc_pdiff'
+
 
 	END program fv3bucket
 
@@ -94,9 +95,10 @@
 C grib2
       INTEGER :: LUGB,LUGI,J,JDISC,JPDTN,JGDTN
       INTEGER,DIMENSION(:) :: JIDS(200),JPDT(200),JGDT(200)
+      logical*1, allocatable:: bmap_f(:)
       LOGICAL :: UNPACK
       INTEGER :: K,IRET
-      TYPE(GRIBFIELD) :: GFLD
+      TYPE(GRIBFIELD) :: GFLD, GFLD_QPF
 C grib2
 !	real:: s_later(IM*JM),s_earlier(IM*JM),sprecip(im*jm)
         real, allocatable :: s_later(:),s_earlier(:)
@@ -108,11 +110,10 @@ C grib2
 
 	write(0,*) 'baopened ', fname1,fname2,testout
 
-	if ( (ierr1+ierr2+ierr3+ierr4) .ne. 0) then
+	if ( (ierr1+ierr2+ierr3) .ne. 0) then
 		write(0,*) 'bad baopen!!! ', ierr1
 		write(0,*) 'bad baopen!!! ', ierr2
 		write(0,*) 'bad baopen!!! ', ierr3
-		write(0,*) 'bad baopen!!! ', ierr4
 		STOP
 	endif
 
@@ -130,13 +131,11 @@ C grib2
         JGDT=-9999
         UNPACK=.true.
 
+        if ( .not. allocated(bmap_f)) then
+        allocate(bmap_f(IM*JM))
+        endif
 
-        allocate(gfld%fld(im*jm))
-        allocate(gfld%idsect(200))
-        allocate(gfld%igdtmpl(200))
-        allocate(gfld%ipdtmpl(200))
-        allocate(gfld%idrtmpl(200))
-        allocate(gfld%bmap(im*jm))
+! -------------------------------
 
         J=0
         JIDS=-9999
@@ -158,7 +157,9 @@ C grib2
         s_earlier=gfld%fld
         write(0,*) 'maxval(s_earlier): ', maxval(s_earlier)
 
+! -------------------------------
 
+        J=0
         JIDS=-9999
         JPDTN=0
         JPDT=-9999
@@ -181,9 +182,36 @@ C grib2
 !        write(0,*) 'ingest J,gfld%ipdtmpl(J): ', J,gfld%ipdtmpl(J)
 !        enddo
 
+        bmap_f=gfld%bmap
+
         write(0,*) 'set s_later to gfld%fld'
         s_later=gfld%fld
         write(0,*) 'maxval(s_later): ', maxval(s_later)
+
+! -------------------------------
+
+! Pull in QPF to get more proper PDS for WEASD bucket
+
+        JIDS=-9999
+        JPDTN=8
+        JPDT=-9999
+        JPDT(2)=8
+        JGDTN=-1
+        JGDT=-9999
+
+        call getgb2(12,0,0,0,JIDS,JPDTN,JPDT,JGDTN,JGDT,
+     &     UNPACK,K,GFLD_QPF,IRET1)
+
+        allocate(gfld_qpf%bmap(im*jm))
+
+        if (iret1 .eq. 0) then
+        gfld_qpf%bmap=bmap_f
+        else
+        write(0,*) 'choked on QPF get: ', iret1
+        endif
+
+! ---------------------------------
+
 
 	if (reset_flag .eq. 1) then
 	write(0,*) 'just later value'
@@ -202,6 +230,7 @@ C grib2
         if (  gfld%bmap(NPT) ) then ! make sure is a valid point
 	sprecip(NPT)=max(s_later(NPT)-s_earlier(NPT),0.0)
         endif
+
 	enddo
 
         write(0,*) 'min,max of sprecip: ', minval(sprecip), 
@@ -212,31 +241,31 @@ C grib2
 	deallocate(s_later)
 	deallocate(s_earlier)
 
-!        do J=1,gfld%ipdtlen
+	write(0,*) 'shape(gfld_qpf%ipdtmpl): ', shape(gfld_qpf%ipdtmpl)
 
-        gfld%ipdtmpl(22)=1
-        gfld%ipdtmpl(27)=interv
+        gfld_qpf%ipdtmpl(22)=1
+        gfld_qpf%ipdtmpl(27)=interv
 
         write(0,*) 'interval specified in 27: ', interv
 
 ! -------------------------------------------
 
 
-       gfld%ipdtmpl(1)=1
-       gfld%ipdtmpl(2)=13
-       gfld%ipdtmpl(3)=2
-       gfld%ipdtmpl(4)=0
-       gfld%ipdtmpl(5)=84
-       gfld%ipdtmpl(6)=0 ! hours cutoff
-       gfld%ipdtmpl(7)=0 ! minutes cutoff
-       gfld%ipdtmpl(8)=1 ! units of hours
-       gfld%ipdtmpl(9)=ihrs1 ! earlier forecast time of period?
-       gfld%ipdtmpl(10)=1 ! sfc
-       gfld%ipdtmpl(11)=0 ! sfc
-       gfld%ipdtmpl(12)=0 ! sfc
-       gfld%ipdtmpl(13)=255 ! sfc
-       gfld%ipdtmpl(14)=0 ! sfc
-       gfld%ipdtmpl(15)=0 ! sfc
+       gfld_qpf%ipdtmpl(1)=1
+       gfld_qpf%ipdtmpl(2)=13
+       gfld_qpf%ipdtmpl(3)=2
+       gfld_qpf%ipdtmpl(4)=0
+       gfld_qpf%ipdtmpl(5)=84
+       gfld_qpf%ipdtmpl(6)=0 ! hours cutoff
+       gfld_qpf%ipdtmpl(7)=0 ! minutes cutoff
+       gfld_qpf%ipdtmpl(8)=1 ! units of hours
+       gfld_qpf%ipdtmpl(9)=ihrs1 ! earlier forecast time of period?
+       gfld_qpf%ipdtmpl(10)=1 ! sfc
+       gfld_qpf%ipdtmpl(11)=0 ! sfc
+       gfld_qpf%ipdtmpl(12)=0 ! sfc
+       gfld_qpf%ipdtmpl(13)=255 ! sfc
+       gfld_qpf%ipdtmpl(14)=0 ! sfc
+       gfld_qpf%ipdtmpl(15)=0 ! sfc
 
 !!! need to figure out how to do this end of period date stuff right
 
@@ -245,54 +274,42 @@ C grib2
 
        write(0,*) 'rinc: ', rinc
        idat=0
-       idat(1)=gfld%idsect(6)
-       idat(2)=gfld%idsect(7)
-       idat(3)=gfld%idsect(8) 
-       idat(5)=gfld%idsect(9)
+       idat(1)=gfld_qpf%idsect(6)
+       idat(2)=gfld_qpf%idsect(7)
+       idat(3)=gfld_qpf%idsect(8) 
+       idat(5)=gfld_qpf%idsect(9)
 
-!	write(0,*) 'idat(1:3),idat(5): ', 
-!     &  idat(1:3),idat(5)
 
        call w3movdat(rinc,idat,jdat)
 
-!	write(0,*) 'jdat(1:3),jdat(5): ', 
-!     &  jdat(1:3),jdat(5)
+       gfld_qpf%ipdtmpl(16)=jdat(1)
+       gfld_qpf%ipdtmpl(17)=jdat(2)
+       gfld_qpf%ipdtmpl(18)=jdat(3)
+       gfld_qpf%ipdtmpl(19)=jdat(5)
+       gfld_qpf%ipdtmpl(20)=0
+       gfld_qpf%ipdtmpl(21)=0
 
-       gfld%ipdtmpl(16)=jdat(1)
-       gfld%ipdtmpl(17)=jdat(2)
-       gfld%ipdtmpl(18)=jdat(3)
-       gfld%ipdtmpl(19)=jdat(5)
-       gfld%ipdtmpl(20)=0
-       gfld%ipdtmpl(21)=0
+        gfld_qpf%ipdtnum=8
+        gfld_qpf%ipdtmpl(22)=1
+        gfld_qpf%ipdtmpl(23)=0
+        gfld_qpf%ipdtmpl(24)=1 ! accum?
+        gfld_qpf%ipdtmpl(25)=2 ! fcst hour increments
+        gfld_qpf%ipdtmpl(26)=1 ! hours?
+        gfld_qpf%ipdtmpl(27)=interv
+        gfld_qpf%ipdtmpl(28)=255
+        gfld_qpf%ipdtmpl(29)=0
 
-        gfld%ipdtnum=8
-        gfld%ipdtmpl(22)=1
-        gfld%ipdtmpl(23)=0
-        gfld%ipdtmpl(24)=1 ! accum?
-        gfld%ipdtmpl(25)=2 ! fcst hour increments
-        gfld%ipdtmpl(26)=1 ! hours?
-        gfld%ipdtmpl(27)=interv
-        gfld%ipdtmpl(28)=255
-        gfld%ipdtmpl(29)=0
+        gfld_qpf%fld=sprecip
 
-!        do J=1,29
-!        write(0,*) 'J,gfld%ipdtmpl(J): ', J,gfld%ipdtmpl(J)
-!        enddo
 
-        gfld%fld=sprecip
-
-!        gfld%idrtmpl(2)=-4
-!        gfld%idrtmpl(3)=0
-!        gfld%idrtmpl(4)=16
-
-	write(0,*) 'use gfld%idrtmpl(1:10): ', gfld%idrtmpl(1:10)
+	write(0,*) 'use gfld_qpf%idrtmpl(1:10): ', gfld_qpf%idrtmpl(1:10)
 
 
 !! use GET_BITS to compute nbits?
 
       IBM=0
       IBITM = 0
-      gfld%idrtmpl(2)=-2.0
+      gfld_qpf%idrtmpl(2)=-2.0
       SGDS  = gfld%idrtmpl(2)
 
       write(0,*) 'here a'
@@ -314,7 +331,7 @@ C grib2
       ELSE
         IBM = 1
       ENDIF
-      call GET_BITS(IBM,SGDS,IM*JM,ibmap,gfld%fld,
+      call GET_BITS(IBM,SGDS,IM*JM,ibmap,gfld_qpf%fld,
      &                ISCALE,GROUND,GMIN,GMAX,NBIT)
 
       write(0,*) 'returned NBIT as: ', NBIT
@@ -323,11 +340,11 @@ C grib2
       write(0,*) 'GMAX: ', GMAX
 
 
-        gfld%idrtmpl(4)=NBIT
+        gfld_qpf%idrtmpl(4)=NBIT
 
 	write(0,*) 'use gfld%idrtmpl(1:10): ', gfld%idrtmpl(1:10)
 
-	call putgb2(13,GFLD,IRET)
+	call putgb2(13,GFLD_QPF,IRET)
         write(0,*) 'IRET from putgb2 for sprecip ', IRET
 
         call baclose(13,IRET)
