5a89224be1a081b266eee3dac7aedb14678273fc
matthew pyle <Matthew.Pyle@noaa.gov>
2020-02-20 22:37:40 +0000

    Large set of changes related to CCPP and compressed netCDF

---
+++ sorc/build_stnmlist.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+
+module purge >& /dev/null
+
+module load ncep
+module load craype-sandybridge
+module use -a /opt/cray/modulefiles
+
+module load -a ../modulefiles/wcoss_cray/v8.0.0-cray-intel
+# module load -a ../modulefiles/wcoss_cray/hiresw_fv3_module
+
+
+module list
+
+cd ./regional_stnmlist.fd
+
+make clean
+
+make
+
+cd ../
+
