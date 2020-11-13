e3c80257ed56015ce93e52c81416bae56da02e4d
matthew pyle <Matthew.Pyle@noaa.gov>
2020-11-13 17:07:52 +0000

    Cleans model out from Externals.cfg file.  Attempts to make overall build_all.sh script Cleans model out from Externals.cfg file.  Attempts to make overall build_all.sh script better for running multiple times (checks on existence of files brought in by link_fix and manage_externals) better for running multiple times (checks on existence of files brought in by link_fix and manage_externals)

--- sorc/build_all.sh
+++ sorc/build_all.sh
@@ -5,12 +5,18 @@
 #################################
 
 # 1st time only
+if [ ! -e fv3/hireswfv3_utils.fd ]
+then
 ./manage_externals/checkout_externals
+fi
 
 cd fv3
 
 # 1st time only
+if [ ! -e ../../fix/fv3 ]
+then
 ./link_fix.sh
+fi
 
 # /gpfs/dell1/nco/ops/nwtest/upgrade_utils.v0.0.2/exec/checkoutsidecompilefiles ./build_all.sh >& build_all_fv3.log
 ./build_all.sh >& build_all_fv3.log
@@ -24,7 +30,10 @@ exit
 cd ../arw
 
 #1st time only
+if [ ! -e ../../fix/arw ]
+then
 ./link_fix.sh
+fi
 
 # /gpfs/dell1/nco/ops/nwtest/upgrade_utils.v0.0.2/exec/checkoutsidecompilefiles ./build_hiresw.sh >& build_all_arw.log
 ./build_hiresw.sh >& build_all_arw.log
